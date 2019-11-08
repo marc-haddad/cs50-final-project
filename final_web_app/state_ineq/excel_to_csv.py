47981677c6ff19e4ef6344c432df503d28fd7d6b
Marc Haddad <52379634+marc-haddad@users.noreply.github.com>
2019-11-08 00:26:02 -0500

    Update excel_to_csv.py

--- final_web_app/state_ineq/excel_to_csv.py
+++ final_web_app/state_ineq/excel_to_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 def main():
 
     data_xls = pd.read_excel("measures-of-state-inc-ineq.xls")
-    data_xls.to_csv("ineq_data.csv", encoding="utf-8")
+    data_xls.to_csv("resources/ineq_data.csv", encoding="utf-8")
 
 if __name__ == "__main__":
     main()
